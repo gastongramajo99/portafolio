88a8c05f57cc93a31f8ea7937a61d0c50aa04204
Gastón Gramajo <gastongramajo1999@gmail.com>
2025-10-17 13:24:47 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+async function copiarTexto() {
+  try {
+    await navigator.clipboard.writeText('gastongramajo1999@gmail.com');
+    alert('¡Gmail copiado al portapapeles!');
+  } catch (err) {
+    alert('Error al copiar texto: ', err);
+  }
+}
+
+const github = document.querySelectorAll('.github')
+
+github.forEach(b => {
+    b.addEventListener('click', () => {
+        window.open('https://github.com/gastongramajo99', '_blank')
+    })
+})
+
+const cv = document.querySelectorAll('.cv')
+
+cv.forEach(b => {
+  b.addEventListener('click', () => {
+    window.open('/assets/curriculum_vitae.pdf', '_blank')
+  })
+})
+
+const gitBoletin = document.querySelector('.gitBoletin')
+
+gitBoletin.addEventListener('click', () => {
+    window.open('https://github.com/gastongramajo99/boletin-oficial', '_blank')
+})
+
+const boletin = document.querySelector('.boletin')
+
+boletin.addEventListener('click', () => {
+    window.open('https://boletin-oficial-6a5h.onrender.com', '_blank')
+})
+
