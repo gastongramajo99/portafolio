fc8bac1ccbaee5e0ce59887c97d06f31dace9d66
gastongramajo99 <gastongramajo1999@gmail.com>
2025-10-17 14:02:27 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@ const cv = document.querySelectorAll('.cv')
 
 cv.forEach(b => {
   b.addEventListener('click', () => {
-    window.open('/portafolio/assets/curriculum_vitae.pdf', '_blank')
+    window.open('https://gastongramajo99.github.io/portafolio/assets/curriculum_vitae.pdf', '_blank')
   })
 })
 
