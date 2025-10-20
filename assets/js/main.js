fa55351f27b72dbc1e551b112c950af9f8fc8d25
Gastón Gramajo <gastongramajo1999@gmail.com>
2025-10-20 13:28:08 -0300

    Actualiza portafolio (HTML, CSS y JS)

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,6 @@ async function copiarTexto() {
 }
 
 const github = document.querySelectorAll('.github')
-
 github.forEach(b => {
     b.addEventListener('click', () => {
         window.open('https://github.com/gastongramajo99', '_blank')
@@ -16,22 +15,23 @@ github.forEach(b => {
 })
 
 const cv = document.querySelectorAll('.cv')
-
 cv.forEach(b => {
   b.addEventListener('click', () => {
     window.open('https://gastongramajo99.github.io/portafolio/assets/curriculum_vitae.pdf', '_blank')
   })
 })
 
-const gitBoletin = document.querySelector('.gitBoletin')
+const imgBoletin = document.querySelector('.img-boletin')
+imgBoletin.addEventListener('click', () => {
+  window.open('https://boletin-oficial-6a5h.onrender.com', '_blank')
+})
 
+const gitBoletin = document.querySelector('.gitBoletin')
 gitBoletin.addEventListener('click', () => {
     window.open('https://github.com/gastongramajo99/boletin-oficial', '_blank')
 })
 
 const boletin = document.querySelector('.boletin')
-
 boletin.addEventListener('click', () => {
     window.open('https://boletin-oficial-6a5h.onrender.com', '_blank')
 })
-
